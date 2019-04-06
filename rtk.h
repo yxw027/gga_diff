d65f22b56e70a4a1180d85c08835f97beadc4ef4
Dr. Yudan Yi <yydgis@gmail.com>
2019-04-05 21:23:04 -0700

    rtk

---
+++ rtk.h
@@ -0,0 +1,269 @@
+#ifndef _RTCM_H_
+#define _RTCM_H_
+
+#include <time.h>
+
+/* by Dr. Yudan Yi */
+
+/*-----------------------------------------------------------*/
+/* from rtklib to decode RTCM3 */
+
+#define NFREQ 2
+#define NEXOBS 0
+
+#define MINPRNGPS   1                   /* min satellite PRN number of GPS */
+#define MAXPRNGPS   35                  /* max satellite PRN number of GPS */
+#define NSATGPS     (MAXPRNGPS-MINPRNGPS+1) /* number of GPS satellites */
+#define NSYSGPS     1
+
+#define MINPRNGLO   1                   /* min satellite slot number of GLONASS */
+#define MAXPRNGLO   30                  /* max satellite slot number of GLONASS */
+#define NSATGLO     (MAXPRNGLO-MINPRNGLO+1) /* number of GLONASS satellites */
+#define NSYSGLO     1
+
+#define MINPRNGAL   1                   /* min satellite PRN number of Galileo */
+#define MAXPRNGAL   35                  /* max satellite PRN number of Galileo */
+#define NSATGAL    (MAXPRNGAL-MINPRNGAL+1) /* number of Galileo satellites */
+#define NSYSGAL     1
+
+#define MINPRNQZS   193                 /* min satellite PRN number of QZSS */
+#define MAXPRNQZS   199                 /* max satellite PRN number of QZSS */
+#define MINPRNQZS_S 183                 /* min satellite PRN number of QZSS SAIF */
+#define MAXPRNQZS_S 189                 /* max satellite PRN number of QZSS SAIF */
+#define NSATQZS     (MAXPRNQZS-MINPRNQZS+1) /* number of QZSS satellites */
+#define NSYSQZS     1
+
+#define MINPRNCMP   1                   /* min satellite sat number of BeiDou */
+#define MAXPRNCMP   35                  /* max satellite sat number of BeiDou */
+#define NSATCMP     (MAXPRNCMP-MINPRNCMP+1) /* number of BeiDou satellites */
+#define NSYSCMP     1
+
+#ifdef ENALEO
+#define MINPRNLEO   1                   /* min satellite sat number of LEO */
+#define MAXPRNLEO   10                  /* max satellite sat number of LEO */
+#define NSATLEO     (MAXPRNLEO-MINPRNLEO+1) /* number of LEO satellites */
+#define NSYSLEO     1
+#else
+#define MINPRNLEO   0
+#define MAXPRNLEO   0
+#define NSATLEO     0
+#define NSYSLEO     0
+#endif
+#define NSYS        (NSYSGPS+NSYSGLO+NSYSGAL+NSYSQZS+NSYSCMP+NSYSLEO) /* number of systems */
+
+#define MINPRNSBS   120                 /* min satellite PRN number of SBAS */
+#define MAXPRNSBS   142                 /* max satellite PRN number of SBAS */
+#define NSATSBS     (MAXPRNSBS-MINPRNSBS+1) /* number of SBAS satellites */
+
+#define MAXSAT      (NSATGPS+NSATGLO+NSATGAL+NSATQZS+NSATCMP+NSATSBS+NSATLEO)
+
+#define MAXOBS 30
+
+#define SYS_NONE    0x00                /* navigation system: none */
+#define SYS_GPS     0x01                /* navigation system: GPS */
+#define SYS_SBS     0x02                /* navigation system: SBAS */
+#define SYS_GLO     0x04                /* navigation system: GLONASS */
+#define SYS_GAL     0x08                /* navigation system: Galileo */
+#define SYS_QZS     0x10                /* navigation system: QZSS */
+#define SYS_CMP     0x20                /* navigation system: BeiDou */
+#define SYS_IRN     0x40                /* navigation system: IRNSS */
+#define SYS_LEO     0x80                /* navigation system: LEO */
+#define SYS_ALL     0xFF                /* navigation system: all */
+
+#define CODE_NONE   0                   /* obs code: none or unknown */
+#define CODE_L1C    1                   /* obs code: L1C/A,G1C/A,E1C (GPS,GLO,GAL,QZS,SBS) */
+#define CODE_L1P    2                   /* obs code: L1P,G1P    (GPS,GLO) */
+#define CODE_L1W    3                   /* obs code: L1 Z-track (GPS) */
+#define CODE_L1Y    4                   /* obs code: L1Y        (GPS) */
+#define CODE_L1M    5                   /* obs code: L1M        (GPS) */
+#define CODE_L1N    6                   /* obs code: L1codeless (GPS) */
+#define CODE_L1S    7                   /* obs code: L1C(D)     (GPS,QZS) */
+#define CODE_L1L    8                   /* obs code: L1C(P)     (GPS,QZS) */
+#define CODE_L1E    9                   /* (not used) */
+#define CODE_L1A    10                  /* obs code: E1A        (GAL) */
+#define CODE_L1B    11                  /* obs code: E1B        (GAL) */
+#define CODE_L1X    12                  /* obs code: E1B+C,L1C(D+P) (GAL,QZS) */
+#define CODE_L1Z    13                  /* obs code: E1A+B+C,L1SAIF (GAL,QZS) */
+#define CODE_L2C    14                  /* obs code: L2C/A,G1C/A (GPS,GLO) */
+#define CODE_L2D    15                  /* obs code: L2 L1C/A-(P2-P1) (GPS) */
+#define CODE_L2S    16                  /* obs code: L2C(M)     (GPS,QZS) */
+#define CODE_L2L    17                  /* obs code: L2C(L)     (GPS,QZS) */
+#define CODE_L2X    18                  /* obs code: L2C(M+L),B1I+Q (GPS,QZS,CMP) */
+#define CODE_L2P    19                  /* obs code: L2P,G2P    (GPS,GLO) */
+#define CODE_L2W    20                  /* obs code: L2 Z-track (GPS) */
+#define CODE_L2Y    21                  /* obs code: L2Y        (GPS) */
+#define CODE_L2M    22                  /* obs code: L2M        (GPS) */
+#define CODE_L2N    23                  /* obs code: L2codeless (GPS) */
+#define CODE_L5I    24                  /* obs code: L5/E5aI    (GPS,GAL,QZS,SBS) */
+#define CODE_L5Q    25                  /* obs code: L5/E5aQ    (GPS,GAL,QZS,SBS) */
+#define CODE_L5X    26                  /* obs code: L5/E5aI+Q/L5B+C (GPS,GAL,QZS,IRN,SBS) */
+#define CODE_L7I    27                  /* obs code: E5bI,B2I   (GAL,CMP) */
+#define CODE_L7Q    28                  /* obs code: E5bQ,B2Q   (GAL,CMP) */
+#define CODE_L7X    29                  /* obs code: E5bI+Q,B2I+Q (GAL,CMP) */
+#define CODE_L6A    30                  /* obs code: E6A        (GAL) */
+#define CODE_L6B    31                  /* obs code: E6B        (GAL) */
+#define CODE_L6C    32                  /* obs code: E6C        (GAL) */
+#define CODE_L6X    33                  /* obs code: E6B+C,LEXS+L,B3I+Q (GAL,QZS,CMP) */
+#define CODE_L6Z    34                  /* obs code: E6A+B+C    (GAL) */
+#define CODE_L6S    35                  /* obs code: LEXS       (QZS) */
+#define CODE_L6L    36                  /* obs code: LEXL       (QZS) */
+#define CODE_L8I    37                  /* obs code: E5(a+b)I   (GAL) */
+#define CODE_L8Q    38                  /* obs code: E5(a+b)Q   (GAL) */
+#define CODE_L8X    39                  /* obs code: E5(a+b)I+Q (GAL) */
+#define CODE_L2I    40                  /* obs code: B1I        (BDS) */
+#define CODE_L2Q    41                  /* obs code: B1Q        (BDS) */
+#define CODE_L6I    42                  /* obs code: B3I        (BDS) */
+#define CODE_L6Q    43                  /* obs code: B3Q        (BDS) */
+#define CODE_L3I    44                  /* obs code: G3I        (GLO) */
+#define CODE_L3Q    45                  /* obs code: G3Q        (GLO) */
+#define CODE_L3X    46                  /* obs code: G3I+Q      (GLO) */
+#define CODE_L1I    47                  /* obs code: B1I        (BDS) */
+#define CODE_L1Q    48                  /* obs code: B1Q        (BDS) */
+#define CODE_L5A    49                  /* obs code: L5A SPS    (IRN) */
+#define CODE_L5B    50                  /* obs code: L5B RS(D)  (IRN) */
+#define CODE_L5C    51                  /* obs code: L5C RS(P)  (IRN) */
+#define CODE_L9A    52                  /* obs code: SA SPS     (IRN) */
+#define CODE_L9B    53                  /* obs code: SB RS(D)   (IRN) */
+#define CODE_L9C    54                  /* obs code: SC RS(P)   (IRN) */
+#define CODE_L9X    55                  /* obs code: SB+C       (IRN) */
+#define MAXCODE     55                  /* max number of obs code */
+
+typedef struct {        /* time struct */
+    time_t time;        /* time (s) expressed by standard time_t */
+    double sec;         /* fraction of second under 1 s */
+} gtime_t;
+
+typedef struct {        /* observation data record */
+    unsigned char sat; /* satellite/receiver number */
+	char freq;
+    unsigned char SNR [NFREQ]; /* signal strength (0.25 dBHz) */
+    unsigned char LLI [NFREQ]; /* loss of lock indicator */
+    unsigned char code[NFREQ]; /* code indicator (CODE_???) */
+    double L[NFREQ]; /* observation data carrier-phase (cycle) */
+    double P[NFREQ]; /* observation data pseudorange (m) */
+    float  D[NFREQ]; /* observation data doppler frequency (Hz) */
+} obsd_t;
+
+typedef struct {        /* observation data */
+    int n;         /* number of obervation data/allocated */
+    obsd_t data[MAXOBS];       /* observation data records */
+} obs_t;
+
+typedef struct {        /* GPS/QZS/GAL broadcast ephemeris type */
+    unsigned char sat;            /* satellite number */
+    int iode,iodc;      /* IODE,IODC */
+    int sva;            /* SV accuracy (URA index) */
+    int svh;            /* SV health (0:ok) */
+    int week;           /* GPS/QZS: gps week, GAL: galileo week */
+    int code;           /* GPS/QZS: code on L2, GAL/CMP: data sources */
+    int flag;           /* GPS/QZS: L2 P data flag, CMP: nav type */
+    gtime_t toe,toc,ttr; /* Toe,Toc,T_trans */
+                        /* SV orbit parameters */
+    double A,e,i0,OMG0,omg,M0,deln,OMGd,idot;
+    double crc,crs,cuc,cus,cic,cis;
+    double toes;        /* Toe (s) in week */
+    double fit;         /* fit interval (h) */
+    double f0,f1,f2;    /* SV clock parameters (af0,af1,af2) */
+    double tgd[4];      /* group delay parameters */
+                        /* GPS/QZS:tgd[0]=TGD */
+                        /* GAL    :tgd[0]=BGD E5a/E1,tgd[1]=BGD E5b/E1 */
+                        /* CMP    :tgd[0]=BGD1,tgd[1]=BGD2 */
+    double Adot,ndot;   /* Adot,ndot for CNAV */
+} eph_t;
+
+typedef struct {        /* GLONASS broadcast ephemeris type */
+    unsigned char sat;            /* satellite number */
+    int iode;           /* IODE (0-6 bit of tb field) */
+    int frq;            /* satellite frequency number */
+    int svh,sva,age;    /* satellite health, accuracy, age of operation */
+    gtime_t toe;        /* epoch of epherides (gpst) */
+    gtime_t tof;        /* message frame time (gpst) */
+    double pos[3];      /* satellite position (ecef) (m) */
+    double vel[3];      /* satellite velocity (ecef) (m/s) */
+    double acc[3];      /* satellite acceleration (ecef) (m/s^2) */
+    double taun,gamn;   /* SV clock bias (s)/relative freq bias */
+    double dtaun;       /* delay between L1 and L2 (s) */
+} geph_t;
+
+typedef struct {        /* SSR correction type */
+    unsigned char sat;
+    gtime_t t0[6];      /* epoch time (GPST) {eph,clk,hrclk,ura,bias,pbias} */
+    double udi[6];      /* SSR update interval (s) */
+    int iod[6];         /* iod ssr {eph,clk,hrclk,ura,bias,pbias} */
+    int iode;           /* issue of data */
+    int iodcrc;         /* issue of data crc for beidou/sbas */
+    int ura;            /* URA indicator */
+    int refd;           /* sat ref datum (0:ITRF,1:regional) */
+    double deph [3];    /* delta orbit {radial,along,cross} (m) */
+    double ddeph[3];    /* dot delta orbit {radial,along,cross} (m/s) */
+    double dclk [3];    /* delta clock {c0,c1,c2} (m,m/s,m/s^2) */
+    double hrclk;       /* high-rate clock corection (m) */
+    //float  cbias[MAXCODE]; /* code biases (m) */
+    //double pbias[MAXCODE]; /* phase biases (m) */
+    //float  stdpb[MAXCODE]; /* std-dev of phase biases (m) */
+    double yaw_ang,yaw_rate; /* yaw angle and yaw rate (deg,deg/s) */
+    unsigned char update; /* update flag (0:no update,1:update) */
+} ssr_t;
+#define MAXEPH 20
+#define MAXSSR 20
+#define MAXEPH_R 10
+typedef struct {        /* navigation data type */
+    unsigned char n;         /* number of broadcast ephemeris */
+    unsigned char ng;       /* number of glonass ephemeris */
+    unsigned char ns;
+    eph_t eph[MAXEPH];         /* GPS/QZS/GAL ephemeris */
+    geph_t geph[MAXEPH_R];       /* GLONASS ephemeris */
+    ssr_t ssr[MAXSSR];  /* output of ssr corrections */
+    unsigned char ephsat;
+} nav_t;
+
+typedef struct {        /* RTCM control struct type */
+    double pos[3];      /* station position (ecef) (m) */
+    int staid;          /* station id */
+    gtime_t time;       /* message time */
+    gtime_t time_s;     /* message start time */
+    obs_t obs;          /* observation data (uncorrected) */
+    nav_t nav;          /* satellite ephemerides */
+    char msmtype[6][128]; /* msm signal types */
+    int obsflag;        /* obs data complete flag (1:ok,0:not complete) */
+    double cp[MAXSAT][NFREQ+NEXOBS]; /* carrier-phase measurement */
+    unsigned short lock[MAXSAT][NFREQ+NEXOBS]; /* lock time */
+    unsigned short loss[MAXSAT][NFREQ+NEXOBS]; /* loss of lock count */
+    gtime_t lltime[MAXSAT][NFREQ+NEXOBS]; /* last lock time */
+    int nbyte;          /* number of bytes in message buffer */ 
+    int nbit;           /* number of bits in word buffer */ 
+    int len;            /* message length (bytes) */
+    int type; /* last rtcm type */
+    unsigned char buff[1200]; /* message buffer */
+} rtcm_t;
+
+#ifdef RTK
+#define MAXSTN 2
+#else
+#define MAXSTN 1
+#endif
+
+typedef struct {        /* RTCM control struct type */
+    rtcm_t rcv[MAXSTN];
+    nav_t  nav;
+} ppprtk_t;
+
+double time2gpst(gtime_t t, int *week);
+gtime_t utc2gpst(gtime_t t);
+
+int satsys(int sat, int *prn);
+
+void trace   (int level, const char *format, ...);
+
+void ecef2pos(const double *r, double *pos);
+
+int satsys(int sat, int *prn);
+
+double time2gpst(gtime_t t, int *week);
+gtime_t timeget(void);
+
+int input_rtcm3(rtcm_t *rtcm, unsigned char data, unsigned int stnID, ppprtk_t *ppprtk);
+
+/*--------------------------------------------------------------------*/
+
+#endif
